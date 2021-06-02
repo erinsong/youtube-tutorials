5a8b59f0f1997b13884e68a860b9a0832c364dcb
Erin <erinsong23@gmail.com>
2021-06-02 00:37:41 -0700

    add js logic

---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,83 @@
+// cache DOM
+let userScore = 0;
+let compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
+}
+
+function convertLetter(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, compChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = `Your ${convertLetter(userChoice)} beat the Comp's ${convertLetter(compChoice)}. You win! :)`;
+    scoreBoard_div.classList.add("green-glow");
+    setTimeout(() => scoreBoard_div.classList.remove("green-glow"), 500);
+}
+
+function lose(userChoice, compChoice) {
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = `Your ${convertLetter(userChoice)} lost to the Comp's ${convertLetter(compChoice)}. You lose! :(`;
+    scoreBoard_div.classList.add("red-glow");
+    setTimeout(() => scoreBoard_div.classList.remove("red-glow"), 500);
+}
+
+function draw(userChoice, compChoice) {
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = `Your ${convertLetter(userChoice)} ties with the Comp's ${convertLetter(compChoice)}. Draw! :|`;
+    scoreBoard_div.classList.add("gray-glow");
+    setTimeout(() => scoreBoard_div.classList.remove("gray-glow"), 500);
+}
+
+function game(userChoice) {
+    const compChoice = getComputerChoice();
+    switch (userChoice + compChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, compChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, compChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, compChoice);
+            break;
+    }
+}
+
+function main() {
+    rock_div.addEventListener("click", () => game("r"));
+    paper_div.addEventListener("click", () => game("p"));
+    scissors_div.addEventListener("click", () => game("s"));
+}
+
+main();
+
+
+
+
+
+
