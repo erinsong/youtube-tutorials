0d2a5f40b82d48ebaa1fa073f37a47866059ceda
Erin Song <erinsong@Erins-MacBook-Air.local>
2022-07-01 19:29:17 -0700

    added task functionality: delete task from task list

--- todo-app/main.js
+++ todo-app/main.js
@@ -57,7 +57,10 @@ window.addEventListener('load', () => {
                 task_input_el.setAttribute("readonly", "readonly");
                 task_edit_el.innerText = "Edit";
             }
+        });
 
-        })
-    })
-})
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+        });
+    });
+});
