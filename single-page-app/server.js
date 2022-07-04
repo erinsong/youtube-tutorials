30192fd160d841aaf42a4eb4d47e599b0546b21f
Erin Song <erinsong@Erins-MacBook-Air.local>
2022-07-04 02:45:14 -0700

    debugged server

--- single-page-app/server.js
+++ single-page-app/server.js
@@ -5,11 +5,14 @@ const path = require("path");
 // create express app
 const app = express();
 
+// serve static content/directory with middleware
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 // any path/route will go through this handler and get sent the index.html file
 // no matter what path sent to web server (post, settings, etc) will get sent to index
 // important for single page application
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 // changed port to 3000 because 5060 didn't connect
